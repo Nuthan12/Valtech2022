0a180ca83d15dbeaf5d74d13671b234f648a5676
Nuthan SML <nuthan.sml@valtech.com>
2022-09-30 16:15:03 +0530

    First Maven Project

---
+++ maven-example/src/main/java/com/valtech/maven/example/App.java
@@ -0,0 +1,13 @@
+package com.valtech.maven.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
